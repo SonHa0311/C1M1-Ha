6ab0d1afb93bae60d1f483592d9cc9d611b05905
SonHa0311 <118738386+SonHa0311@users.noreply.github.com>
2023-12-25 13:45:42 +0700

    Add files via upload

---
+++ stas.c
@@ -0,0 +1,185 @@
+
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stas.c
+ * @brief this program can be used to find the minimum, maximum, median, mean values and sort array
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Ha Tien Son
+ * @date  25/12/2023
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+
+
+
+
+
+ 
+
+
+int main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+  
+  //prints unsorted data array.
+  printf("******** Array ***********\n");                                
+  print_array(test ,SIZE );
+
+  // Declaring minimum value and print it.
+  unsigned char minimum = find_minimum(test ,SIZE );
+  printf("%u is the minimum value in data array\n",minimum );
+
+  // Declaring maximum value and print it.
+  unsigned char maximum = find_maximum(test ,SIZE );
+  printf("%u is the maximum value in data array\n",maximum );
+
+
+
+  // Declaring mean and print it.
+  unsigned char mean = find_mean(test ,SIZE );
+  printf("%u is the mean in data array\n",mean );
+
+  // Declaring mean and print it.
+  unsigned char median = find_median(test ,SIZE );
+  printf("%u is the median in data array\n",median );
+  
+
+  // Declaring sortedData and print it.
+  printf("******* Sorted Array *********\n");
+  sort_array(test ,SIZE );
+  print_array(test , SIZE );
+
+  return 0;
+
+}
+/*
+/* Add other Implementation File Code Here */
+
+
+void print_array( unsigned char * data , int dataLength )
+{
+	for (int i = 0; i < dataLength; ++i)
+	{
+		printf("%u\n",data[i] );
+	}
+}
+
+
+unsigned char find_median( unsigned char * data , int dataLength )
+{
+
+	//For finding median, array must be sorted
+	sort_array(data, dataLength);
+	unsigned char median;
+
+	int middleData = dataLength / 2 ;
+	//Length of array is even ,then we have to mean of middle items.
+	if( 0 == dataLength % 2 )
+	{
+		median = (data[middleData - 1 ] + data[middleData ] ) / 2 ;	
+	}
+	//Length of array is odd ,then we choose on the middle item. 
+	else
+	{
+		median = data[middleData];
+	}
+	return median;
+}
+
+
+unsigned char find_mean( unsigned char * data , int dataLength )
+{
+	int sum = 0;
+	for (int i = 0; i < dataLength; ++i)
+	{
+		sum += data[i];
+	}
+	return ( sum / dataLength);
+}
+
+
+unsigned find_maximum( unsigned char * data , int dataLength )
+{
+	unsigned char maximum = data[0];
+
+	for (int i = 1; i < dataLength; ++i)
+	{
+		if (maximum < data[i])
+		{
+			maximum = data[i];
+		}
+	}
+	return maximum;
+}
+
+unsigned char find_minimum( unsigned char * data , int dataLength )
+{
+	unsigned char minimum = data[0];
+
+	for (int i = 1; i < dataLength; ++i)
+	{
+		if (minimum > data[i])
+		{
+			minimum = data[i];
+		}
+	}
+	return minimum;
+
+}
+
+void swap( unsigned char * firstItem , unsigned char * secondItem )
+{
+	unsigned char temp = *firstItem;
+	*firstItem = *secondItem;
+	*secondItem = temp;
+}
+
+// A function to implement bubble sort 
+void sort_array( unsigned char * data , int dataLength )
+{
+   int i, j; 
+   int flag; 
+   for (i = 0; i < dataLength-1; i++) 
+   { 
+     flag = 0; 
+     for (j = 0; j < dataLength-i-1; j++) 
+     { 
+        if (data[j] < data[j+1]) 
+        { 
+           swap(&data[j], &data[j+1]); 
+           flag = 1; 
+        } 
+     } 
+  
+     // IF no two elements were swapped by inner loop, then break 
+     if (flag == 0) 
+        break; 
+   } 
+}
---
+++ stats.h
@@ -0,0 +1,139 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Ha Tien Son
+ * @date 12/25/2023
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+void print_array( unsigned char * data , int dataLength );
+
+/**
+ * @brief Prints data array 
+ *
+ * Given an array of data and a length, prints the array to the screen.
+ * Function uses a loop and prints the array.
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Nothing. Because does not have to return anything. It just print an array.
+ */
+
+
+
+unsigned char find_median( unsigned char * sortedData , int dataLength );
+
+/**
+ * @brief Finds data's median value.
+ *
+ * Given an sorted array of data and a length, returns the median value
+ * Function takes sorted data. If data length is odd number then median 
+ * is in the middle of sorted array. If data length is not odd then 
+ * median is the mean of middle data. 
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Median value which is integer type(Rounded value).
+ */
+                                         
+
+
+
+unsigned char find_mean( unsigned char * data , int dataLength );
+
+/**
+ * @brief Finds data's mean value.
+ *
+ * Given an array of data and a length, returns the mean.
+ * Mean is the sum of data divided the data length.
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Mean which is integer type(Rounded value).
+ */
+                                         
+
+
+
+unsigned find_maximum( unsigned char * data , int dataLength );
+/**
+ * @brief Finds the maximum value in the data.
+ *
+ *  Given an array of data and a length, returns the maximum.
+ *	For find the maximum value we will use the linear search.
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Maximum value which is integer type.
+ */
+
+
+
+
+unsigned char find_minimum( unsigned char * data , int dataLength );
+/**
+ * @brief Finds the minimum value in the data.
+ *
+ *  Given an array of data and a length, returns the minimum.
+ *  For find the minimum value we will use the linear search.
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Minimum value which is integer type.
+ */
+
+
+
+void sort_array( unsigned char * data , int dataLength );
+/**
+ * @brief Sorts largest to smalleset.
+ *
+ *  Given an array of data and a length, sorts the array from largest 
+ *  to smallest. (The zeroth Element should be the largest value, 
+ *  and the last element (n-1) should be the smallest value. )
+ *	We will use buble sort method.
+ *
+ * @param unsigned char * data - Input is the data array 
+ * @param int dataLength  - Input is length of the data array
+
+ * @return Array which is sorted and unsigned char type.
+ */
+
+void swap( unsigned char * firstItem , unsigned char * secondItem );
+/**
+ * @brief Swaps two unsigned char .
+ *
+ *  Swaps two unsigned char accessing adresses of items. It is called
+ *  pass by reference.
+ *
+ * @param unsigned char * firstItem - first element to swapped 
+ * @param unsigned char * secondItem  - second element to swapped
+
+ * @return Nothing. Because does not have to return anything.
+ */
+
+
+
+
+#endif /* __STATS_H__ */
